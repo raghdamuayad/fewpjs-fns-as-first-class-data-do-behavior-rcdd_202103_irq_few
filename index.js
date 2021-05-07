f00c683c857ccced4bd8db8b0eb449a0607322b3
Raghda  Muayad <therose000777@gmail.com>
2021-05-07 07:52:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ document.querySelector("button").addEventListener("click", handleClick);
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
+
+}
+function greet() {
+    var greetingone;
+    var time = new Date().getHours();
+    if (time < 12) {
+        greeting = "Good morning";
+    } else if (time < 17) {
+        greeting = "Good Afternoon";
+    } else {
+        greeting = "Good evening";
+    }
+document.getElementById("greeting").innerHTML = greeting;
+console.log(greet);
 }
 
 /* End Given Code, don't edit above here...*/
