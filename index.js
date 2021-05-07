120c8ce23ed79b167cf35e8d23e81380b0d915d0
Raghda  Muayad <therose000777@gmail.com>
2021-05-07 07:56:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* Given Code, don't edit */
 
 function greet(timeAll){
-  let nowTime = timeAll.split(':');
-  let nowHour = parseInt(nowTime[0]);
+  let NowTime = timeAll.split(':');
+  let NowHour = parseInt(nowTime[0]);
   if (nowHour<12){
     return "Good Morning";
   } else if (nowHour>17){
